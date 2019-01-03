0094e2cd6c5301698fc468981932be2f2f999042
Takashi Oya <>
2019-01-03 18:23:51 +0900

    update save_features_for_yuval.py

--- scripts/save_features_for_yuval.py
+++ scripts/save_features_for_yuval.py
@@ -9,9 +9,19 @@ def main():
     np.save('../features/mamas_feat_v1_train.npy', X)
     np.save('../features/mamas_feat_v1_test.npy', X_test)
 
+    fn_s = np.load('../fi/mamas_feature_names_v2.npy')
+    X = load_arr(fn_s, 'train')
+    X_test = load_arr(fn_s, 'test')
+    np.save('../features/mamas_feat_v2_train.npy', X)
+    np.save('../features/mamas_feat_v2_test.npy', X_test)
+
+    fn_s = np.load('../fi/mamas_feature_names_v2.npy')
+    X = load_arr(fn_s, 'train')
+    X_test = load_arr(fn_s, 'test')
+    np.save('../features/mamas_feat_v2_train.npy', X)
+    np.save('../features/mamas_feat_v2_test.npy', X_test)
 
 if __name__ == '__main__':
     main()
 
 
-    
