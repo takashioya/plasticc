89e28307b710de0f2a54c2657ff330417ff67afa
Takashi Oya <>
2019-01-03 17:05:33 +0900

    add save_features_for_yuval.py

---
+++ scripts/save_features_for_yuval.py
@@ -0,0 +1,17 @@
+from utils import *
+import numpy as np
+
+
+def main():
+    fn_s = np.load('../fi/mamas_feature_names_v1.npy')
+    X = load_arr(fn_s, 'train')
+    X_test = load_arr(fn_s, 'test')
+    np.save('../features/mamas_feat_v1_train.npy', X)
+    np.save('../features/mamas_feat_v1_test.npy', X_test)
+
+
+if __name__ == '__main__':
+    main()
+
+
+    
